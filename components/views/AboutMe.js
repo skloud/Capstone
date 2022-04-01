5d8d43581756721014e39bd040273d85fc8e7745
Robyn Mason <mnorbyn@gmail.com>
2022-03-31 19:24:10 -0500

    changed background

--- components/views/AboutMe.js
+++ components/views/AboutMe.js
@@ -4,5 +4,5 @@ import html from "html-literal";
 export default () => html`
 <section id="me">
 <h2>About This Project</h2>
-<p> Info About Project.</p>
+<p> This is my project for the SavvyCoders Jan 2022 Cohort. I wanted to build a website highlighting something that interested me. Going to brunch with friends and family is one of my favorite activities. I searched and realized that there aren't that many sites that are dedicated to providing information only about brunch locations and providing reviews. So low and behold All About Brunch began. </p>
 </section>`;
